7851cea6334d8f34bf3e9af90381b67068fd4ea7
Vladimir Nybozhinsky <skylifewww@gmail.com>
2026-04-20 18:42:24 +0300

    Fix pillow-heif compilation for Render

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 set -o errexit
 
 echo "Installing dependencies..."
+# Install Willow without heif first to avoid compilation issues
+pip install Willow==1.6.3 Pillow==10.4.0
 pip install -r requirements.txt
 
 echo "Collecting static files..."
